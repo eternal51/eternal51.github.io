ab5a8dc4f69ea2382ec3f99f0f09ee4f7ff0de3e
eternal51 <eternal120823@gmail.com>
2023-07-24 06:58:33 +0700

    menghapus beberapa baris komentar

--- dist/js/main.js
+++ dist/js/main.js
@@ -52,13 +52,11 @@ if (urlParams.has("to")) {
 }
 
 // Kategori Tamu VIP
-// const kategori = urlParams.get("v");
 const tamuVip = [ 
   "Alif Fahmi", 
   "Era Lestari"
 ];
 if (tamuVip.includes(tujuan)) {
-// if (urlParams.has("v")) {
   document.querySelector("#kategori-tamu").innerHTML =
     "<span class='text-xl font-semibold py-1 px-4 mx-2 rounded-md text-white bg-sage-5 font-serif-3 items-center'>VIP</span>";
 } else {
