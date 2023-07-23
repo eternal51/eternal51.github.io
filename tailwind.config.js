93d4beee84f4b52f15df16e33d2c58795faff044
eternal51 <eternal120823@gmail.com>
2023-07-23 08:56:31 +0700

    mengubah animasi pada nama Era & Alif

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,76 @@ module.exports = {
       screens: {
         "2xl": "1320px",
       },
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
+        e: {
+          "0%, 100%": { transform: "translateX(1.5rem)" },
+          "50%": { transform: "translateX(3rem)" },
+        },
+        emd: {
+          "0%, 100%": { transform: "translateX(1.5rem)" },
+          "50%": { transform: "translateX(4rem)" },
+        },
+        ra: {
+          "0%, 100%": { transform: "translateX(1.5rem) scale(1)" },
+          "20%, 80%": {
+            transform: "translateX(1.5rem) scale(.002)",
+          },
+          "50%": { transform: "scale(0)" },
+        },
+        tern: {
+          "0%, 100%": { transform: "scale(0)" },
+          "20%, 80%": { transform: "scale(.002)" },
+          "50%": { transform: "translateX(1rem) scale(1)" },
+        },
+        dan: {
+          "0%, 100%": { transform: "translateX(-2rem)" },
+          "20%, 80%": {
+            transform: "translateX(-2rem) scale(.002)",
+          },
+          "50%": { transform: "scale(0)" },
+        },
+        danmd: {
+          "0%, 100%": { transform: "translateX(-2.5rem)" },
+          "20%, 80%": {
+            transform: "translateX(-2.5rem) scale(.002)",
+          },
+          "50%": { transform: "scale(0)" },
+        },
+        al: {
+          "0%, 100%": { transform: "translateX(-1.5rem)" },
+          "50%": {
+            transform: "translateX(-2rem)",
+          },
+        },
+        almd: {
+          "0%, 100%": { transform: "translateX(-1.5rem)" },
+          "50%": {
+            transform: "translateX(-3rem)",
+          },
+        },
+        if: {
+          "0%, 100%": { transform: "translateX(-1.5rem)" },
+          "20%, 80%": {
+            transform: "translateX(-1.5rem) scale(.002)",
+          },
+          "50%": { transform: "scale(0)" },
+        },
+      },
+      animation: {
+        e: "e 6s ease-in-out infinite",
+        emd: "emd 6s ease-in-out infinite",
+        ra: "ra 6s ease-in-out infinite",
+        tern: "tern 6s ease-in-out infinite",
+        dan: "dan 6s ease-in-out infinite",
+        danmd: "dan 6s ease-in-out infinite",
+        al: "al 6s ease-in-out infinite",
+        almd: "almd 6s ease-in-out infinite",
+        if: "if 6s ease-in-out infinite",
+      },
     },
   },
   plugins: [],
