a0c3578e058d9feae65a2c7b0852fa64fa085e60
eternal51 <eternal120823@gmail.com>
2023-07-23 09:32:52 +0700

    mengubah style kategori tamu

--- dist/js/main.js
+++ dist/js/main.js
@@ -54,7 +54,7 @@ if (urlParams.has("to")) {
 const kategori = urlParams.get("v");
 if (urlParams.has("v")) {
   document.querySelector("#kategori-tamu").innerHTML =
-    "<span class='mx-2 rounded-md bg-merah-2 px-6 py-3 text-2xl font-bold tracking-widest text-white font-serif-3'>VIP</span>";
+    "<span class='text-2xl font-bold py-1 px-2 mx-2 rounded-md text-white bg-merah-2 font-serif-3'>VIP</span>";
 } else {
   document.querySelector("#kategori-tamu").innerHTML = "";
 }
